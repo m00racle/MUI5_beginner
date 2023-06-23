df4dd3684322b5c5a5e2fe138feb752131a94ebf
mooracle <yanuar.prakosa@gmail.com>
2023-06-23 10:36:47 +0700

    building material ui layout

---
+++ src/components/TourCard.js
@@ -0,0 +1,11 @@
+
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+export default function TourCard() {
+    return (
+    <Grid item xs={3}>
+        <Paper elevation={3} >Hello Card</Paper>
+    </Grid>
+    );
+}
